6a24dfde25efe4766701c2997179bbdbe288ac55
0ventoaster <zhexxalharvey@gmail.com>
2024-01-24 14:00:47 +0800

    Front End(Base)
    not sure what to add and still stuck on dropdown menu

---
+++ FrontEnd/ADET/AddScheduleWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿// AddScheduleWindow.xaml.cs
+using System.Windows;
+
+namespace ADET
+{
+    public partial class AddScheduleWindow : Window
+    {
+        public AddScheduleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged_1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FrontEnd/ADET/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADET
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddScheduleWindow addScheduleWindow = new AddScheduleWindow();
+        
+            // Show the new window
+            addScheduleWindow.Show();
+        }
+    }
+}
